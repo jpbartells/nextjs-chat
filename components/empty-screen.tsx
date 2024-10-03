750502b986012a11d58bdf4bbcc9ee2c5ed3747a
jpbartells <33104188+jpbartells@users.noreply.github.com>
2024-10-03 16:55:23 -0500

    Update empty-screen.tsx

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -14,15 +14,6 @@ export function EmptyScreen() {
         <p className="leading-normal text-muted-foreground">
           This is an open source AI chatbot app template.
         </p>
-        <p className="leading-normal text-muted-foreground">
-          It uses{' '}
-          <ExternalLink href="https://vercel.com/blog/ai-sdk-3-generative-ui">
-            React Server Components
-          </ExternalLink>{' '}
-          to combine text with generative UI as output of the LLM. The UI state
-          is synced through the SDK so the model is aware of your interactions
-          as they happen.
-        </p>
       </div>
     </div>
   )
